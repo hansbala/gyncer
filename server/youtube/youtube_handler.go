76d2a38e8417bf2c2cf26143c0327e8f392baced
Hans <hansbala@hansbala.com>
2024-06-03 19:49:03 -1000

    boilerplate code for youtube oauth2

---
+++ server/youtube/youtube_handler.go
@@ -0,0 +1,89 @@
+package youtube
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hansbala/gyncer/config"
+	"github.com/hansbala/gyncer/database"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/youtube/v3"
+)
+
+const (
+	// TODO: This should correspond to a unique session
+	cState = "abc123"
+)
+
+var oauth2Config *oauth2.Config
+
+func getOauth2Config() *oauth2.Config {
+	if oauth2Config != nil {
+		return oauth2Config
+	}
+	youtubeConfig := config.GetConfig().Youtube
+	oauth2Config = &oauth2.Config{
+		ClientID:     youtubeConfig.GyncerClientId,
+		ClientSecret: youtubeConfig.GyncerClientSecret,
+		RedirectURL:  youtubeConfig.GyncerRedirectUrl,
+		Scopes:       []string{youtube.YoutubeScope},
+	}
+	return oauth2Config
+}
+
+// Generates a auth url for the user to visit.
+func CreateAuthUrlHandler(c *gin.Context) {
+	oauth2Config := getOauth2Config()
+	authUrl := oauth2Config.AuthCodeURL(cState, oauth2.AccessTypeOffline)
+	c.JSON(http.StatusOK, gin.H{"auth_url": authUrl})
+}
+
+type authenticateUserRequest struct {
+	UserId string `json:"user_id"`
+}
+
+// Authenticates the user and saves the credentials to the database.
+func AuthenticateUserHandler(c *gin.Context) {
+	// extract user id from request
+	var request authenticateUserRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "malformed request"})
+		return
+	}
+
+	// get auth token from request query params
+	oauth2Config := getOauth2Config()
+	token, err := oauth2Config.Exchange(c.Request.Context(), request.Code)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "could not get token"})
+		return
+	}
+
+	client := oauth2Config.Client(context.Background(), token)
+	service, err := youtube.New(client)
+	// check if all is okay - this should be an authenticated client
+	_, err = service.Channels.List("snippet").Do()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "could not get current user" + err.Error()})
+		return
+	}
+
+	// save the credentials to database
+	youtubeCredentials := database.YoutubeCredential{
+		Token:  token,
+		UserId: request.UserId,
+	}
+	db, err := database.ConnectToDB()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "could not connect to database"})
+		return
+	}
+	if err = database.SaveYoutubeCredentials(db, youtubeCredentials); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "could not save credentials to database" + err.Error()})
+		return
+	}
+
+	// all okay - send a 200
+	c.Status(http.StatusOK)
+}
